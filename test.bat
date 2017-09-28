29af5e42b14181aa906d8faf52465a7a8c04bf61
prikh <31078983+prikh@users.noreply.github.com>
2017-09-28 13:46:43 -0400

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,6 +1,6 @@
 @echo off
-set src_folder=C:\Users\priya.khodwe\Google Drive\Personal\Pictures
-set dst_folder='C:\Users\priya.khodwe\Desktop\target'
+set src_folder=C:\Users\<username>\Google Drive\Personal\Pictures
+set dst_folder='C:\Users\<username>\Desktop\target'
 for /f "tokens=*" %%i in (MyFileList.txt) DO (
     xcopy "%src_folder%\%%i" target 
 )
